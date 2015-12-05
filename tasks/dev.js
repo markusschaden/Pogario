3df0ff5f43b8a0e90fa660e0aaca041c1efac18d
David Knezić <davidknezic@gmail.com>
2015-12-05 19:02:47 +0100

    Fix serve task

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,8 +1,14 @@
 import gulp from 'gulp'
 import watch from 'gulp-watch'
+import webserver from 'gulp-webserver'
 
 gulp.task('dev', ['build'], function (cb) {
-  gulp.start('serve')
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      host: '0.0.0.0',
+      port: 3000
+    }))
 
   watch('src/**/*.js', () => {
     gulp.start('scripts')
--- tasks/serve.js
+++ tasks/serve.js
@@ -4,9 +4,7 @@ import webserver from 'gulp-webserver'
 gulp.task('serve', function () {
   gulp.src('dist')
     .pipe(webserver({
-      livereload: true,
       host: '0.0.0.0',
-      port: 3000,
-      fallback: 'index.html'
+      port: 3000
     }))
 })
