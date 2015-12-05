c0a849514bfffffed51e3bdaf000afd1c562f333
David Knezić <davidknezic@gmail.com>
2015-12-05 17:35:36 +0100

    Add canvas, game loop

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import _ from 'lodash'
 import Trianglify from 'trianglify'
+
+//import Player from './player'
+
+let canvasWrapper = document.getElementById("canvasWrapper");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+  canvas.width = canvasWrapper.clientWidth;
+  canvas.height = canvasWrapper.clientHeight;
+}
+resizeCanvas();
+
+
+var thisTime = new Date().getTime();
+var delta = 0;
+var lastTime = thisTime;
+var interval = 0;
+var targetInterval = 30;
+
+//https://github.com/joebain/uprok/blob/master/scripts/flyrock.js#L1098
+function loop() {
+  thisTime = new Date().getTime();
+	delta = thisTime-lastTime;
+	lastTime = thisTime;
+
+	//update(delta);
+	//draw();
+
+	interval = targetInterval - (new Date().getTime() - thisTime);
+	interval = interval < 1 ? 1 : interval;
+	setTimeout(loop, interval);
+}
+
+loop();
