1fb0d9d150f37a47c17c6decb4fcb8dfa6093d0e
David Knezić <davidknezic@gmail.com>
2015-12-05 18:07:55 +0100

    Don't stop build after error, notify on errors

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,22 +5,20 @@ import sourcemaps from 'gulp-sourcemaps'
 import gutil from 'gulp-util'
 import buffer from 'vinyl-buffer'
 import shim from 'browserify-shim'
+import notifier from 'node-notifier'
 import source from 'vinyl-source-stream'
 
 gulp.task('scripts', function () {
   return browserify({
-      entries: 'src/index.js',
       debug: true,
     })
     .transform(babelify, {
       presets: ['es2015']
     })
     .transform(shim)
+    .add('src/index.js')
     .bundle()
-    .pipe(source('index.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .on('error', (err) => {
+    .on('error', function (err) {
       gutil.log(err.message)
       notifier.notify({
         title: 'Building Scripts Failed',
@@ -28,6 +26,12 @@ gulp.task('scripts', function () {
       })
       this.emit('end')
     })
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    on('error', gutil.log)
     .pipe(sourcemaps.write(''))
     .pipe(gulp.dest('dist'))
 })
